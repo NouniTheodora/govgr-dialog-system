74da5baef9cfe062d7a14d8f60fdbc3d1c9a7cf7
Theodora Nouni <nounitheodora@gmail.com>
2025-04-01 00:44:44 +0300

    Added: Translations of the main page

--- js/change-language-functions.js
+++ js/change-language-functions.js
@@ -1,10 +1,10 @@
 var languageContent = {
     greek: {
       languageBtn: "EL",
-      mainTitle: "Δελτίο Μετακίνησης ΑΜΕΑ",
-      pageTitle: "Δελτίο Μετακίνησης ΑΜΕΑ",
-      infoTitle: "Πληροφορίες για την χορήγηση Δελτίων Μετακίνησης ΑΜΕΑ 2023",
-      subTitle1: "Αυτό το ερωτηματολόγιο μπορεί να σας βοηθήσει να βρείτε αν δικαιούστε να λάβετε το δελτίο μετακίνησης ΑΜΕΑ.",
+      mainTitle: "Άδεια Ορκωτού Ελεγκτή Λογιστή",
+      pageTitle: "Άδεια Ορκωτού Ελεγκτή Λογιστή",
+      infoTitle: "Πληροφορίες για την χορήγηση επαγγελματικής άδειας σε Ορκωτούς Ελεγκτές Λογιστές",
+      subTitle1: "Αυτό το ερωτηματολόγιο μπορεί να σας βοηθήσει να διαπιστώσετε αν πληροίτε τις προϋποθέσεις για τη χορήγηση άδειας Ορκωτού Ελεγκτή Λογιστή από την Ε.Λ.Τ.Ε.",
       subTitle2: "H συμπλήρωση του ερωτηματολογίου δεν απαιτεί παραπάνω από 10 λεπτά.",
       subTitle3: "Δεν θα αποθηκεύσουμε ούτε θα μοιραστούμε τις απαντήσεις σας.",
       backButton: "Πίσω",
@@ -14,10 +14,8 @@ var languageContent = {
       readAloud: "Ανάγνωση",
       changeContrast: "Αντίθεση",
       readingMask: "Μάσκα Ανάγνωσης",
-      footerText: "Αυτό το έργο δημιουργήθηκε για τις ανάγκες της πτυχιακής μας εργασίας κατά τη διάρκεια των προπτυχιακών μας σπουδών στο Πανεπιστήμιο Μακεδονίας. Η ομάδα μας αποτελείται από 2 φοιτήτριες της Εφαρμοσμένης Πληροφορικής:",
+      footerText: "Υλοποίηση για το μάθημα Συστήματα Ηλεκτρονικής Διακυβέρνησης (NG0204), Πανεπιστήμιο Μακεδονίας, 2025, Νούνη Θεοδώρα - mai25040",
       and: "και",
-      student1: "Αμπατζίδου Ελισάβετ",
-      student2: "Δασύρα Ευμορφία Ελπίδα",
       startBtn:"Ας ξεκινήσουμε",
       chooseAnswer: "Επιλέξτε την απάντησή σας",
       oneAnswer: "Μπορείτε να επιλέξετε μόνο μία επιλογή.",
@@ -26,32 +24,29 @@ var languageContent = {
     },
     english: {
       languageBtn: "EN",
-      mainTitle: "Transportation Card for the Disabled",
-      pageTitle: "Transportation Card for the Disabled",
-      infoTitle: "Information on the issue of Transportation Cards for the Disabled 2023",
-      subTitle1: "This questionnaire can help you determine if you are eligible to receive the transportation card for the Disabled.",
-      subTitle2: "Completing the questionnaire should not take more than 10 minutes.",
-      subTitle3: "We will neither store nor share your answers.",
-      backButton: "Βack",
-      nextQuestion: "Next Question",
+      mainTitle: "Certified Public Accountant License",
+      pageTitle: "Certified Public Accountant License",
+      infoTitle: "Information regarding the granting of a professional license to Certified Public Accountants.",
+      subTitle1: "This questionnaire can help you determine if you meet the requirements for obtaining a Certified Public Accountant license from the Hellenic Accounting and Auditing Standards Oversight Board (ELTE).",
+      subTitle2: "Filling out the questionnaire should take no more than 10 minutes.",
+      subTitle3: "We do not store or share your answers.",
+      backButton: "Back",
+      nextQuestion: "Next question",
       biggerCursor: "Bigger Cursor",
-      bigFontSize:" Big Font Size",
+      bigFontSize: "Big Font Size",
       readAloud: "Read Aloud",
-      changeContrast:" Change Contrast",
-      readingMask:" Reading Mask",
-      footerText: "This project was created for the needs of our thesis at the University of Macedonia. Our team consists of 2 students of Applied Informatics:",
+      changeContrast: "Change Contrast",
+      readingMask: "Reading Mask",
+      footerText: "Developed for the course Electronic Governance Systems (NG0204), University of Macedonia, 2025, Nouni Theodora - mai25040",
       and: "and",
-      student1: "Ampatzidou Elisavet",
-      student2: "Dasyra Evmorfia Elpida",
-      startBtn:"Let's start",
+      startBtn: "Let's start",
       chooseAnswer: "Choose your answer",
-      oneAnswer: "You can choose only one option.",
+      oneAnswer: "You can select only one option.",
       errorAn: "Error:",
-      choose: "You must choose one option",
+      choose: "You must choose an answer",
     }
 };
   
-// Retrieve the selected language from localStorage or set default to "greek"
 var currentLanguage = localStorage.getItem("preferredLanguage") || "greek";
 
 function toggleLanguage() {
@@ -69,5 +64,4 @@ function updateContent() {
     });
 }
 
-// Initialize the content based on the selected language
 updateContent();
